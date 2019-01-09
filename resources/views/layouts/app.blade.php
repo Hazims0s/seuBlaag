24a90e25ead779c214eab32c82a5ea671346c68a
Hazim Alraddadi <hazim.rd@gmail.com>
2019-01-09 03:54:35 +0300

    Alpha timed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Seu Reporter') }}</title>
+    <title>Seu Reporter</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
